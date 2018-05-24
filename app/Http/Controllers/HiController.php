9d1978b1c60be7af2ea6ddbe88b5adcf1878ebaf
bestzzintheworld <748372584@qq.com>
2018-05-24 19:07:02 +0800

    hello

--- app/Http/Controllers/HiController.php
+++ app/Http/Controllers/HiController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Hi;
 use App\Swoole\Server;
 use App\Swoole\Client;
+use Illuminate\Http\Request;
+
 
 class HiController extends UserController
 {
@@ -17,34 +19,14 @@ class HiController extends UserController
         phpinfo();
     }
 
-    public function testServer(){
-        //创建Server对象，监听 127.0.0.1:9501端口
-        $serv = new swoole_server("127.0.0.1", 9501);
-
-//监听连接进入事件
-        $serv->on('connect', function ($serv, $fd) {
-            echo "Client: Connect.\n";
-        });
-
-//监听数据接收事件
-        $serv->on('receive', function ($serv, $fd, $from_id, $data) {
-            $serv->send($fd, "Server: ".$data);
-        });
-
-//监听连接关闭事件
-        $serv->on('close', function ($serv, $fd) {
-            echo "Client: Close.\n";
-        });
-
-//启动服务器
-        $serv->start();
+    public function hello(){
 
     }
 
-    public function testClient(){
-        $client = new Client();
-        $client->connect();
+    public function upload(Request $request){
+        $path = $request->file('avatar')->store('avatars');
 
+        return $path;
     }
 }
 
---
+++ resources/views/Hi/helllo.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>
+        This is hello view!
+    </title>
+</head>
+<body>
+        welcome to the hello view!
+</body>
+</html>
